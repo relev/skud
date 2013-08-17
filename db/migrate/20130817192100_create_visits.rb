0ce563f9c13b57546f119ec1ed78ac2ff315711e
lev <relevv@gmail.com>
2013-08-17 23:47:31 +0400

    улучшил порядок в миграциях

--- db/migrate/20130817192100_create_visits.rb
+++ db/migrate/20130817192100_create_visits.rb
@@ -1,9 +1,9 @@
 class CreateVisits < ActiveRecord::Migration
   def change
     create_table :visits do |t|
+      t.references :user, index: true
       t.references :device, index: true
       t.integer :session_key
-      t.references :user, index: true
       t.timestamp :closed_at
 
       t.timestamps
--- db/migrate/20130817192705_create_devices.rb
+++ db/migrate/20130817192705_create_devices.rb
@@ -2,9 +2,9 @@ class CreateDevices < ActiveRecord::Migration
   def change
     create_table :devices do |t|
       t.references :device_type, index: true
-      t.string :name
       t.references :user, index: true
       t.integer :session_key
+      t.string :name
       t.string :device_key
 
       t.timestamps
