0ece411a7881640e601472b406476920fe20e586
amorroma <gafiatullin.roman@gmail.com>
2013-08-17 17:28:55 +0400

    развернул рельсы

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skud::Application.config.secret_key_base = '3e6e109870067dc4cb2840189bf0062b6d5a6c7d58769ccb48c2c1f6213bc1dcb585c370f8cda59f31763096955df292512bdb06ea0ee2703f42254cf29a06b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skud::Application.config.session_store :cookie_store, key: '_skud_session'
