ee83d6668fc9847b4d3e3ddb97fecafbde3aa1d0
Aidar <aidar@inbox.com>
2013-08-17 16:43:24 -0700

    добавил сидов

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,37 @@ DeviceType.create( name: 'person.mobile.android' )
 DeviceType.create( name: 'person.card' )
 DeviceType.create( name: 'anonymous.alarmclock' )
 
+semen=User.create(
+  email: 'semen@yandex.ru',
+  password: '123123',
+  first_name: 'Семён',
+  last_name: 'Борода',
+  gender: 1,
+  birth_date: '1970-01-15')
+
+alena=User.create(
+  email: 'alena@yandex.ru',
+  password: '555333',
+  first_name: 'Алёна',
+  last_name: 'Сливина',
+  gender: 0,
+  birth_date: '1990-04-12')
+
+dev1=Device.create(
+      device_type_id: 1,
+      user: semen,
+      name: 'Semens device',
+      device_key: 'JKSAHDKJsd-09jhj'
+)
+Visit.create(
+      user: semen,
+      device: dev1,
+      session_key: 1,
+      closed_at: 1376785433
+)
+Visit.create(
+      user_id: 1,
+      device: dev1,
+      session_key: 2,
+      closed_at: 1376789433
+)
